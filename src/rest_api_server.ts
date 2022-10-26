d8800551f147a9cb53ff4f829bb6ace23c4342b9
EgorShiryaev <shiryaevvyatsu@gmail.com>
2022-10-26 18:55:41 +0300

    rename methods

--- src/rest_api_server.ts
+++ src/rest_api_server.ts
@@ -1,15 +1,15 @@
-import express from 'express';
-import authHandler from './server_method_handlers/auth_handler';
-import registrationHandler from './server_method_handlers/registration_handler';
-import favoriteInstrumentsHandler from './server_method_handlers/favorite_instruments_handler';
-import addFavoriteInstrumentsHandler from './server_method_handlers/add_favorite_instruments_handler';
-import deleteFavoriteInstrumentsHandler from './server_method_handlers/delete_favorite_instruments_handler';
-import searchInstrumentsHandler from './server_method_handlers/search_instruments_handler';
-import UserTableManager from './managers/user_table_manager';
-import InstrumentTableManager from './managers/instrument_table_manager';
-import FavoriteInstrumentsTableManager from './managers/favorite_intruments_table_manager';
-import loadInstruments from './usecases/load_instruments/load_instruments';
-import Settings from '../settings';
+import express from "express";
+import authHandler from "./server_method_handlers/auth_handler";
+import registrationHandler from "./server_method_handlers/registration_handler";
+import favoriteInstrumentsHandler from "./server_method_handlers/favorite_instruments_handler";
+import addFavoriteInstrumentsHandler from "./server_method_handlers/add_favorite_instruments_handler";
+import deleteFavoriteInstrumentsHandler from "./server_method_handlers/delete_favorite_instruments_handler";
+import searchInstrumentsHandler from "./server_method_handlers/search_instruments_handler";
+import UserTableManager from "./managers/user_table_manager";
+import InstrumentTableManager from "./managers/instrument_table_manager";
+import FavoriteInstrumentsTableManager from "./managers/favorite_intruments_table_manager";
+import loadInstruments from "./usecases/load_instruments/load_instruments";
+import Settings from "../settings";
 
 const createTables = () => {
   return Promise.all([
@@ -26,10 +26,10 @@ const initServer = () => {
     .then(loadInstruments)
     .then(() => app.listen(Settings.serverPort, Settings.serverUrl))
     .then(() => {
-      console.log('Success start server');
+      console.log("Success start server");
     })
     .catch((error) => {
-      console.log('Fail start server', error);
+      console.log("Fail start server", error);
     });
 
   return app;
@@ -38,20 +38,16 @@ const initServer = () => {
 const server = initServer();
 const jsonParser = express.json();
 
-server.post('/registration', jsonParser, registrationHandler);
-server.post('/auth', jsonParser, authHandler);
+server.post("/registration", jsonParser, registrationHandler);
+server.post("/auth", jsonParser, authHandler);
 
-const favoriteInstrumentsPath = '/favoriteInstruments';
+const favoriteInstrumentsPath = "/favoriteInstruments";
 server.get(favoriteInstrumentsPath, favoriteInstrumentsHandler);
-server.post(
-  `${favoriteInstrumentsPath}/add`,
-  jsonParser,
-  addFavoriteInstrumentsHandler
-);
+server.post(favoriteInstrumentsPath, jsonParser, addFavoriteInstrumentsHandler);
 server.delete(
-  `${favoriteInstrumentsPath}/delete`,
+  favoriteInstrumentsPath,
   jsonParser,
   deleteFavoriteInstrumentsHandler
 );
 
-server.get('/instruments/search', searchInstrumentsHandler);
+server.get("/instruments/search", searchInstrumentsHandler);
